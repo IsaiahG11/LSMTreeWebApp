9647e84da4a451173fa56c3be3cd16dc7a781cc8
Dyl-Miller <dylmil2001@hotmail.com>
2023-10-05 18:43:26 -0400

    sstable compaction added
    Co-authored-by: IsaiahG11 <IsaiahG11@users.noreply.github.com>

--- backend/memTable.js
+++ backend/memTable.js
@@ -10,7 +10,7 @@ const SSTable = require('./ssTable'); // Import the SSTable module
 class SkipList{
     constructor(head = null){
         this.head = head;
-        this.memTableSize = 0; //
+        this.memTableSize = 0; //Size of the list
     }
 
 
@@ -60,7 +60,7 @@ class SkipList{
     
         let currentNode = this.head;
         while (currentNode) {
-            dataToWrite.push([currentNode.data.key, currentNode.data.value]);
+            dataToWrite.push([currentNode.key, currentNode.getValue()]);
             currentNode = currentNode.next;
         }
     
@@ -105,24 +105,35 @@ class ListNode{
 const sstable = new SSTable('my_sstable.txt');
 
 var list = new SkipList();
-var node0 = new ListNode("value0", 324);
+var node0 = new ListNode("value0", 0);
 list.insertNode(node0);
 console.log("Added node0")
 
-var node1 = new ListNode("value1", 400);
+var node1 = new ListNode("value1", 1);
 list.insertNode(node1);
 console.log("Added node1")
 
-var node2 = new ListNode("value2", 123);
+var node2 = new ListNode("value2", 2);
 list.insertNode(node2);
 console.log("Added node2")
 
-var node3 = new ListNode("value3", 7);
+var node3 = new ListNode("value3", 3);
 list.insertNode(node3);
 console.log("Added node3")
 
-var node4 = new ListNode("value4", 1);
+var node4 = new ListNode("value4", 4);
 list.insertNode(node4);
 
+var node5 = new ListNode("value5", 5);
+list.insertNode(node5);
+
+var node6 = new ListNode("value6", 6);
+list.insertNode(node6);
+
+var node7 = new ListNode("value7", 7);
+list.insertNode(node7);
+
+var node8 = new ListNode("value8", 8);
+list.insertNode(node8);
 
 list.printList();
--- backend/ssTable.js
+++ backend/ssTable.js
@@ -9,8 +9,10 @@ const fs = require('fs');
 // Define a simple SSTable class
 class SSTable {
   constructor(filename) {
+    this.serCount = 0;
     this.filename = filename;
     this.data = new Map();
+    this.ext = "txt";
   }
 
   // Insert a key-value pair into the SSTable
@@ -19,6 +21,7 @@ class SSTable {
   }
 
   insertBulk(entries) {
+
     for (const [key, value] of entries) {
         this.data.set(key, value);
     }
@@ -33,14 +36,54 @@ class SSTable {
   // Serialize the SSTable to a file
   serialize() {
     const serializedData = JSON.stringify(Array.from(this.data.entries()));
-    fs.writeFileSync(this.filename, serializedData, 'utf8');
+    fs.writeFileSync(this.filename + this.serCount, serializedData, 'utf8');
+    if(this.serCount > 0){
+      this.compact();
+    }
+    this.serCount++;
+    this.data.clear();
   }
 
   // Deserialize the SSTable from a file
   deserialize() {
-    const fileData = fs.readFileSync(this.filename, 'utf8');
+    const fileData = fs.readFileSync(this.filename + this.serCount, 'utf8');
     const entries = JSON.parse(fileData);
     this.data = new Map(entries);
+  }
+
+  compact(){
+    var serCountMinus = this.serCount - 1;
+
+    const newFileData = fs.readFileSync(this.filename + this.serCount, 'utf8');
+    const oldFileData = fs.readFileSync(this.filename + serCountMinus, 'utf8');
+
+    let newer = JSON.parse(newFileData);
+    let old = JSON.parse(oldFileData);
+    
+    console.log(newer);
+    console.log(old);
+
+
+    //array.push(JSON.parse(newFileData));
+    //array.push(JSON.parse(oldFileData));
+
+    //var serializedData = "";
+
+    for (const [key, value] of old) {
+      newer.push([key, value]);
+    }
+
+    console.log(newer);
+
+    var serializedData = "";
+    for(const [key, value] of newer){
+      serializedData += JSON.stringify([key, value]);
+    }
+
+    console.log(serializedData);
+    
+    
+    fs.writeFileSync(this.filename + this.serCount, serializedData, 'utf8');
   }
 }
 
