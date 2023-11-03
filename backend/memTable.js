f4d0e2205183765c1862018fc83b84e508d0353f
Dylan Miller <dylmil2001@hotmail.com>
2023-11-03 12:30:13 -0400

    search function added

--- backend/memTable.js
+++ backend/memTable.js
@@ -22,7 +22,7 @@ class MemTable{
     //Initializes each layer with a negative bound. (All inserted values must be 0 or positive)
     init(){
 
-        var negNode = new ListNode("negBound", -1);
+        let negNode = new ListNode("negBound", -1);
 
         this.head = negNode;
 
@@ -35,38 +35,39 @@ class MemTable{
         }
     }
 
-    // Finds where the node needs to be inserted to keep ordering
-    findInsertLocation(node){
-        var tmp = this.head;
-        var tmpNext = null;
-        var tmpPrev = null;
-        if(tmp.next != null){
-            tmpNext = tmp.next;
-        }else{
-            return tmp;
-        }
-        while(node.getValue() > tmp.next.getValue()){
-            if(tmpNext.next == null){
-                tmp = tmpNext;
-                break;
-            }else{
-                tmpNext = tmpNext.next;
-                tmp = tmp.next;
-            }
-        }
-        return tmp;
-    }
+    // TODO NOT CURRENTLY USED (can probably delete)
+    // // Finds where the node needs to be inserted to keep ordering
+    // findInsertLocation(node){
+    //     var tmp = this.head;
+    //     var tmpNext = null;
+    //     var tmpPrev = null;
+    //     if(tmp.next != null){
+    //         tmpNext = tmp.next;
+    //     }else{
+    //         return tmp;
+    //     }
+    //     while(node.getValue() > tmp.next.getValue()){
+    //         if(tmpNext.next == null){
+    //             tmp = tmpNext;
+    //             break;
+    //         }else{
+    //             tmpNext = tmpNext.next;
+    //             tmp = tmp.next;
+    //         }
+    //     }
+    //     return tmp;
+    // }
 
-    findNodesBefore(node){
+    // findNodesBefore(node){
 
-    }
+    // }
 
     // Inserts the node into the memtable
     insertNode(node){
 
-        var currNode = this.head;
+        let currNode = this.head;
 
-        var nodesBeforeInsert = [];
+        let nodesBeforeInsert = [];
 
 
         while(currNode != null){ //may need to change condition
@@ -83,9 +84,9 @@ class MemTable{
             }
         }
 
-        var willPromote = true;
+        let willPromote = true;
 
-        var downNode = null;
+        let downNode = null;
 
         while(nodesBeforeInsert.length != 0 && willPromote){
 
@@ -109,7 +110,7 @@ class MemTable{
 
 
 
-
+    // TODO NOT CURRENTLY USED (can probably delete)
     /**
 
         if(this.head.next == null){
@@ -141,6 +142,26 @@ class MemTable{
         */
     }
 
+    // Search for a node by value
+search(value) {
+    let currentNode = this.head;
+    let searching = true;
+    while (searching) {
+        if (currentNode.data.has(currentNode.key) && currentNode.data.get(currentNode.key) === value) {
+            return currentNode;
+        } else if (currentNode.next?.data.has(currentNode.next?.key) && currentNode.next?.data.get(currentNode.next?.key) <= value) {
+            currentNode = currentNode.next;
+        } else if (currentNode.down) {
+            currentNode = currentNode.down;
+        } else {
+            searching = false;
+        }
+    }
+
+    return null; // Node with the given value not found
+}
+
+
     // Saves the memtable to an sstable
     writeMemTableToSSTable() {
         const dataToWrite = [];
@@ -162,7 +183,7 @@ class MemTable{
 
     // TESTING PURPOSES, to see what is in our memtable
     printList(){
-        var tmp = this.head;
+        let tmp = this.head;
         console.log(tmp);
         while(tmp.next != null){
             tmp = tmp.next;
@@ -170,9 +191,10 @@ class MemTable{
         }
     }
 
+    // TESTING PURPOSES, prints visual skiplist w/ layers
     printLayers(){
-        var layerString = "\n";
-        var node = this.head
+        let layerString = "\n";
+        let node = this.head
         while(node != null){
             layerString += node.getValue();
             let tmp = node;
@@ -194,11 +216,31 @@ class MemTable{
 module.exports = MemTable; // Export the SkipList class
 
 
-var list = new MemTable();
+let list = new MemTable();
 
 list.insertNode(new ListNode("key", 1));
-
+list.insertNode(new ListNode("key", 4));
+list.insertNode(new ListNode("key", 2));
+list.insertNode(new ListNode("key", 5));
 list.printLayers();
+
+const foundNode = list.search(2);
+
+if (foundNode) {
+    console.log("Found node:", foundNode.getValue());
+} else {
+    console.log("Node not found.");
+}
+
+const foundAnothaNode = list.search(5);
+
+if (foundAnothaNode) {
+    console.log("Found node:", foundAnothaNode.getValue());
+} else {
+    console.log("Node not found.");
+}
+
+
 
 
 /**
@@ -214,45 +256,4 @@ var node1 = new ListNode("value1", 1);
 console.log("Adding node1")
 list.insertNode(node1);
 
-
-var node2 = new ListNode("value2", 2);
-console.log("Adding node2")
-list.insertNode(node2);
-
-
-var node3 = new ListNode("value3", 3);
-console.log("Adding node3")
-list.insertNode(node3);
-
-
-var node4 = new ListNode("value4", 4);
-console.log("Adding node4")
-list.insertNode(node4);
-
-
-var node5 = new ListNode("value5", 5);
-console.log("Adding node5")
-list.insertNode(node5);
-
-
-var node6 = new ListNode("value6", 6);
-console.log("Adding node6")
-list.insertNode(node6);
-
-
-var node7 = new ListNode("value7", 7);
-console.log("Adding node7")
-list.insertNode(node7);
-
-
-var node8 = new ListNode("value8", 8);
-console.log("Adding node8")
-list.insertNode(node8);
-
-console.log();
-
-list.printList();
-
 */
-
-
